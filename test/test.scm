834fc014fc3c1f25ad0a03f05ee77e34d7b3060a
Chunye Wang <chunye.wang@amd.com>
2024-05-08 09:47:17 +0800

    rename os to rime-98

--- test/test.scm
+++ test/test.scm
@@ -2,6 +2,6 @@
 (import (rnrs (6))
         (test repl)
         (rime rime-0)
-        (rime os environ))
-
+        (rime rime-98 __get-environment-variable))
+(display (string-append "HOME=" (get-environment-variable "HOME") "\n"))
 (main)
